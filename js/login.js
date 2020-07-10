5b594ca6faf6cc5a1dc1297cc79a6afb86104ecb
FernandoMed18 <fernando2013mdina@gmail.com>
2020-07-10 01:21:58 -0600

    ahi esta el login

--- js/login.js
+++ js/login.js
@@ -1,5 +1,8 @@
 let tab1 = document.querySelector("#tab1");
 let tab2 = document.querySelector("#tab2");
+let inputs = document.getElementsByClassName("formulario");
+let label = document.getElementsByClassName("label");
+
 
 function cambiarColor(ref) {
     if (ref.getAttribute("data-tab") == "login") {
@@ -23,6 +26,29 @@ function changeTab(ref) {
         } else {
             document.getElementById("form-body").classList.add('active');
             ref.parentNode.classList.add('signup');
+        }
+    } catch (msg) {
+        console.log(msg);
+    }
+}
+
+function changeTabPago(ref) {
+    try {
+        if (ref.getAttribute("data-tab") == "direccion") {
+            document.getElementById("form-body").classList.remove('active');
+            document.getElementById("form-body").classList.remove('visited');
+            ref.parentNode.classList.remove('page2');
+            ref.parentNode.classList.remove('page3');
+        } else if (ref.getAttribute("data-tab") == "pago") {
+            document.getElementById("form-body").classList.remove('visited');
+            ref.parentNode.classList.add('page2');
+            ref.parentNode.classList.remove('page1');
+            ref.parentNode.classList.remove('page3');
+        } else {
+            document.getElementById("form-body").classList.add('active');
+            ref.parentNode.classList.add('page3');
+            ref.parentNode.classList.remove('page1');
+            ref.parentNode.classList.remove('page2');
         }
     } catch (msg) {
         console.log(msg);
