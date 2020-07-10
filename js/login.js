1524cac4964edb262c64d592ef084d756343caf6
FernandoMed18 <fernando2013mdina@gmail.com>
2020-07-10 05:48:09 -0600

    pago listo

--- js/login.js
+++ js/login.js
@@ -1,5 +1,9 @@
 let tab1 = document.querySelector("#tab1");
 let tab2 = document.querySelector("#tab2");
+let tab_1 = document.querySelector("#tab_1");
+let tab = document.querySelector("#tab_1").style;
+let tab_2 = document.querySelector("#tab_2");
+let tab_3 = document.querySelector("#tab_3");
 let inputs = document.getElementsByClassName("formulario");
 let label = document.getElementsByClassName("label");
 
@@ -32,25 +36,99 @@ function changeTab(ref) {
     }
 }
 
-function changeTabPago(ref) {
+// function changeTabPago(ref) {
+//     try {
+//         if (ref.getAttribute("data-tab") == "direccion") {
+//             document.getElementById("form-body").classList.remove('active');
+//             document.getElementById("form-body").classList.remove('active1');
+//             ref.parentNode.classList.remove('page');
+//             ref.parentNode.classList.remove('page');
+//         }
+
+//         if (ref.getAttribute("data-tab") == "pago" ) {
+//             document.getElementById("form-body").classList.add('active');
+//             document.getElementById("form-body").classList.remove('active1');
+//             ref.parentNode.classList.remove('page');
+//             ref.parentNode.classList.remove('page');
+//         } 
+
+//         else {
+//             document.getElementById("form-body").classList.add('active1');
+//             ref.parentNode.classList.add('page');
+//             ref.parentNode.classList.remove('page');
+//         }
+//     } catch (msg) {
+//         console.log(msg);
+//     }
+// }
+
+function tabb1(ref){
+    document.getElementById("form-body").classList.remove('active');
+    document.getElementById("form-body").classList.remove('active1');
+    ref.parentNode.classList.remove('page');
+    ref.parentNode.classList.remove('page');
+    tab_1.style.color = "gray";
+    tab_1.style.borderBottom = "3px solid gray";
+    tab_2.style.color = "#d5d4d6";
+    tab_2.style.border = "none";
+    tab_3.style.color = "#d5d4d6";
+    tab_3.style.border = "none";
+}
+
+function tabb2(ref){
+    document.getElementById("form-body").classList.add('active');
+    document.getElementById("form-body").classList.remove('active1');
+    ref.parentNode.classList.remove('page');
+    ref.parentNode.classList.remove('page');
+    tab_2.style.color = "gray";
+    tab_2.style.borderBottom = "3px solid gray";
+    tab_1.style.color = "#d5d4d6";
+    tab_1.style.border = "none";
+    tab_3.style.color = "#d5d4d6";
+    tab_3.style.border = "none";
+}
+
+function tabb3(ref) {
     try {
-        if (ref.getAttribute("data-tab") == "direccion") {
-            document.getElementById("form-body").classList.remove('active');
-            document.getElementById("form-body").classList.remove('visited');
-            ref.parentNode.classList.remove('page2');
-            ref.parentNode.classList.remove('page3');
-        } else if (ref.getAttribute("data-tab") == "pago") {
-            document.getElementById("form-body").classList.remove('visited');
-            ref.parentNode.classList.add('page2');
-            ref.parentNode.classList.remove('page1');
-            ref.parentNode.classList.remove('page3');
-        } else {
-            document.getElementById("form-body").classList.add('active');
-            ref.parentNode.classList.add('page3');
-            ref.parentNode.classList.remove('page1');
-            ref.parentNode.classList.remove('page2');
-        }
+
+            document.getElementById("form-body").classList.add('active1');
+            ref.parentNode.classList.add('page');
+            ref.parentNode.classList.remove('page');
+            tab_3.style.color = "gray";
+            tab_3.style.borderBottom = "3px solid gray";
+            tab_1.style.color = "#d5d4d6";
+            tab_1.style.border = "none";
+            tab_2.style.color = "#d5d4d6";
+            tab_2.style.border = "none";
+
     } catch (msg) {
         console.log(msg);
+    }
+}
+
+function cambiarColorPago(ref) {
+    if (ref.getAttribute("data-tab") == "direccion") {
+        tab_1.style.color = "gray";
+        tab_1.style.borderBottom = "3px solid gray";
+        tab_2.style.color = "#d5d4d6";
+        tab_2.style.border = "none";
+        tab_3.style.color = "#d5d4d6";
+        tab_3.style.border = "none";
+    }
+    else if (ref.getAttribute("data-tab") == "pago") {
+        tab_2.style.color = "gray";
+        tab_2.style.borderBottom = "3px solid gray";
+        tab_1.style.color = "#d5d4d6";
+        tab_1.style.border = "none";
+        tab_3.style.color = "#d5d4d6";
+        tab_3.style.border = "none";
+    } 
+    else {
+        tab_3.style.color = "gray";
+        tab_3.style.borderBottom = "3px solid gray";
+        tab_1.style.color = "#d5d4d6";
+        tab_1.style.border = "none";
+        tab_2.style.color = "#d5d4d6";
+        tab_2.style.border = "none";
     }
 }
